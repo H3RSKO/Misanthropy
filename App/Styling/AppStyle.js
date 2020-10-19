78b82d52ff03550ffdc89422f869cc63cc4e2b10
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-10-19 16:16:44 -0400

    button works. would like to increase width to show side of image

--- App/Styling/AppStyle.js
+++ App/Styling/AppStyle.js
@@ -1,10 +1,18 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import '../../public/fonts.css'
+import button11 from '../../public/images/button11.png'
 
 const theme = createMuiTheme({
   typography: {
     fontFamily: "Piazzolla",
     fontSize: 19,
+    button: {
+      backgroundImage: 'url(' + button11 + ')',
+      'background-repeat': 'no-repeat',
+      // 'background-attachment': 'fixed',
+      'background-position': 'center',
+      'background-size': 'cover'
+    },
   },
   // possibly superfluous 👇
   overrides: {
