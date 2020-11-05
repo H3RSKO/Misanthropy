2b9909460c0d82bbb343ca0b6e0311c3bcf7e3a9
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-11-05 14:13:18 -0500

    added popup dialog todo

---
+++ App/Components/SignedIn.js
@@ -0,0 +1,3 @@
+// import React from 'react';
+// import Dialog from '@material-ui/core/Dialog'; see https://material-ui.com/components/dialogs/
+// a popup after succesfu,lly creating account or logging in
