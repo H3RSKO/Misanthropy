b114565cc8b205979dbf1d0241f3ea36bb4c3e61
Yosef Herskovitz <yosefherskovitz@gmail.com>
2021-01-28 18:18:06 -0500

    1

--- App/Pages/NewThread.js
+++ App/Pages/NewThread.js
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import {Editor, EditorState, RichUtils} from 'draft-js';
-import 'draft-js/dist/Draft.css';
+// import {Editor, EditorState, RichUtils} from 'draft-js';
+import Editor, { createEditorStateWithText } from '@draft-js-plugins/editor';
+import createToolbarPlugin, {Separator} from '@draft-js-plugins/static-toolbar';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from '@draft-js-plugins/buttons';
+import editorStyles from './editorStyles.module.css'
+// import 'draft-js/dist/Draft.css';
 import newThreadStyle from '../Styling/NewThreadStyle'
 
 
