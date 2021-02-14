1634d6f648b521e5a8443eaa1a5e4f5e50938d9e
Yosef Herskovitz <yosefherskovitz@gmail.com>
2021-02-14 14:32:51 -0500

    styled texteeditor component

--- App/Components/TextEditor/TextEditor.js
+++ App/Components/TextEditor/TextEditor.js
@@ -15,7 +15,7 @@ const TextEditor = (props) => {
                 <CKEditor
                     className={classes.CKEditor}
                     editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
+                    data=""
                     onReady={ editor => {
                         // You can store the "editor" and use when it is needed.
                         console.log( 'Editor is ready to use!', editor );
